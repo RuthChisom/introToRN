0f3b8d3194ac20124a9de7e47a0a6e4869befc1f
Ruth Chisom <ndukwe100805043@gmail.com>
2023-04-11 10:50:44 +0100

    add more input fields to my form and submit as object for later backend use

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, Button , ScrollView} from 'react-native';
 import Section2 from './components/Section2';
 import styles from './styles'
 import FormInput from './components/FormInput';
@@ -33,47 +33,48 @@ export default function App() {
 
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        {/* header text */}
-        <View style={styles.head}>
-          <Text style={styles.headerText}>Welcome to MY Intro to React Native</Text>
+      <ScrollView>
+        <View style={styles.container} >
+          {/* header text */}
+          <View style={styles.head}>
+            <Text style={styles.headerText}>Welcome to MY Intro to React Native</Text>
+          </View>
+
+          {/* paragraph */}
+          <View style={styles.paragraph}>
+            <Text style={styles.pText}>This is going to be fun!!</Text>
+            <StatusBar style="auto" />
+          </View>
+
+          {/* image */}
+          <View style={styles.myImgContainer}>
+            <Image
+            style={styles.myImg}
+            source={require("./assets/logo-react-icon.png")}
+            resizeMode="contain"
+            />
+          </View>
+
+          {/* button */}
+          <View style={styles.btnArea}>
+            <TouchableOpacity onPress={accessMyContacts} style={styles.btn}>
+              <Text style={styles.btnText}>Access My Contact</Text>
+            </TouchableOpacity>
+
+          </View>
+
+          <Button title='Happy Birthday' onPress={happyBirthday} />
+
+          <Text>{age} Years Old</Text>
+
+          <Section title="Section 1"/>
+          
+          <Section2 title="Section 2"/>
+
+          <FormInput />
+          
         </View>
-
-        {/* paragraph */}
-        <View style={styles.paragraph}>
-          <Text style={styles.pText}>This is going to be fun!!</Text>
-          <StatusBar style="auto" />
-        </View>
-
-        {/* image */}
-        <View style={styles.myImgContainer}>
-          <Image
-           style={styles.myImg}
-          source={require("./assets/logo-react-icon.png")}
-          resizeMode="contain"
-          />
-        </View>
-
-        {/* button */}
-        <View style={styles.btnArea}>
-          <TouchableOpacity onPress={accessMyContacts} style={styles.btn}>
-            <Text style={styles.btnText}>Access My Contact</Text>
-          </TouchableOpacity>
-
-        </View>
-
-        <Button title='Happy Birthday' onPress={happyBirthday} />
-
-        <Text>{age} Years Old</Text>
-
-        <Section title="Section 1"/>
-        
-        <Section2 title="Section 2"/>
-
-        <FormInput />
-        
-      </View>
-      
+      </ScrollView>
     </SafeAreaView>
 
     
--- components/FormInput.js
+++ components/FormInput.js
@@ -1,30 +1,81 @@
-import { View, Text, TextInput, Alert } from 'react-native'
+import { View, Text, TextInput, Alert, Button } from 'react-native'
 import React, { useState } from 'react'
 
 const FormInput = () => {
     // define name as hook, so that input will show in textinput box
     const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const submitData = () => {
+        let myInfo = {
+            name,
+            email,
+            phone
+        }
+        console.log("Data Submitted", myInfo);
+    }
 
   return (
     <View style={{margin: 15}}>
+        {/* Name */}
       <TextInput
-    //   value={name}
-      onChangeText={setName}
-      placeholder={'Please enter your Fullname '}
-      placeholderTextColor={{color:'red'}}
-      style={{borderWidth: 1,padding: 10, borderColor: 'red', borderRadius: 20}}
-    //   keyboardType="numeric"
-    // onPressIn = {() => Alert.alert("Your details will be Protected!!")}
-    onPressOut = {() => Alert.alert("Continue Typing!!")}
+        //   value={name}
+        onChangeText={setName}
+        placeholder={'Please enter your Fullname '}
+        placeholderTextColor={{color:'red'}}
+        style={{borderWidth: 1,padding: 10, borderColor: 'red', borderRadius: 20}}
+        // onPressIn = {() => Alert.alert("Your details will be Protected!!")}
+        onPressOut = {() => Alert.alert("Continue Typing!!")}
       />
+
+      {/* Email */}
+      <TextInput
+        onChangeText={setEmail}
+        placeholder={'Please enter your Email Address '}
+        placeholderTextColor={{color:'red'}}
+        style={{borderWidth: 1,padding: 10, borderColor: 'red', borderRadius: 20, marginTop: 5}}
+        keyboardType="email-address"    
+      />
+
+      {/* Phone Number */}
+      <TextInput
+        onChangeText={setPhone}
+        placeholder={'Please enter your Phone Number '}
+        placeholderTextColor={{color:'red'}}
+        style={{borderWidth: 1,padding: 10, borderColor: 'red', borderRadius: 20, marginTop: 5}}
+        keyboardType="phone-pad"   
+      />
+
       <View style={{margin: 10, flexDirection: 'row', justifyContent: "space-between"}}>
         <View style={{justifyContent:'center'}}>
-            <Text style={{fontSize: 10}}>Your Name is:</Text>
+            <Text style={{fontSize: 10}}> Name:</Text>
         </View>
         <View style={{flex:1, paddingLeft:15, justifyContent:'center'}}>
             <Text style={{fontSize: 10, color: 'purple'}}>{name}</Text>
         </View>
       </View>
+
+      <View style={{margin: 10, flexDirection: 'row', justifyContent: "space-between"}}>
+        <View style={{justifyContent:'center'}}>
+            <Text style={{fontSize: 10}}>Email:</Text>
+        </View>
+        <View style={{flex:1, paddingLeft:15, justifyContent:'center'}}>
+            <Text style={{fontSize: 10, color: 'purple'}}>{email}</Text>
+        </View>
+      </View>
+
+      <View style={{margin: 10, flexDirection: 'row', justifyContent: "space-between"}}>
+        <View style={{justifyContent:'center'}}>
+            <Text style={{fontSize: 10}}>Phone:</Text>
+        </View>
+        <View style={{flex:1, paddingLeft:15, justifyContent:'center'}}>
+            <Text style={{fontSize: 10, color: 'purple'}}>{phone}</Text>
+        </View>
+      </View>
+
+      <Button title='Submit' onPress={submitData} />
+
     </View>
   )
 }
