415ab4523241cd8aa390fb5ce85bcf5a99e8854a
Ruth Chisom <ndukwe100805043@gmail.com>
2023-04-18 12:15:57 +0100

    conditional rendering
    Method 1 .... logical operator
    Method 2 ... tenary operator
    Method 3 ... if/else

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import FormInput from './components/FormInput';
 import {names} from './externalData';
 import Users from './components/Users';
 
+// child component
 const Section = (propss) => {
   return (
     <View style={{margin : 10}}>
@@ -15,6 +16,46 @@ const Section = (propss) => {
   )
 }
 
+// child component
+const Card = ({title, showButton, buttonTitle}) => {
+  return (
+    <View 
+      style={{
+        margin: 20,
+        height: 100,
+        borderRadius:8,
+        backgroundColor: 'lavender',
+        padding:12,
+        justifyContent:'center',
+      }}
+    >
+      <Text style={{fontSize: 15, fontWeight: 500}}>{title}</Text>
+      {/* this will only be displayed if showButton is true */}
+      {/* Method 1 */}
+      {showButton && <Button title='Click Me'/>}
+      {/* Method 2 */}
+      {buttonTitle ? <Button title={buttonTitle}/> : null}
+    </View>
+  )
+}
+
+// child component to display message to user
+const UserDisplay = ({loading, error, title}) => {
+  let showContent;
+
+  {/* Method 3 */}
+  if(error) showContent = <Text style={{color:'red'}}>Something went wrong!</Text>
+  else if(loading) showContent = <Text style={{color:'blue'}}>Loading...</Text>
+  else showContent = <Text style={{color:'green'}}>{title}</Text>
+
+  return(
+    <View style={{padding:20}}>
+      <Text>{showContent}</Text>
+    </View>
+  )
+}
+
+// parent component
 export default function App() {
 
   const [name, setName] = useState('Ruth');
@@ -36,11 +77,64 @@ export default function App() {
   return (
     <SafeAreaView>
     {/* <SafeAreaView style={{flex:1}}> */}
-    
+      <ScrollView>
+        <View style={styles.container} >
+          {/* header text */}
+          <View style={styles.head}>
+            <Text style={styles.headerText}>Welcome to MY Intro to React Native</Text>
+          </View>
+
+          {/* paragraph */}
+          <View style={styles.paragraph}>
+            <Text style={styles.pText}>This is going to be fun!!</Text>
+            <StatusBar style="auto" />
+          </View>
+
+          {/* image */}
+          <View style={styles.myImgContainer}>
+            <Image
+            style={styles.myImg}
+            source={require("./assets/logo-react-icon.png")}
+            resizeMode="contain"
+            />
+          </View>
+
+          {/* button */}
+          <View style={styles.btnArea}>
+            <TouchableOpacity onPress={accessMyContacts} style={styles.btn}>
+              <Text style={styles.btnText}>Access My Contact</Text>
+            </TouchableOpacity>
+
+          </View>
+
+          <Button title='Happy Birthday' onPress={happyBirthday} />
+
+          <Text>{age} Years Old</Text>
+
+          <Section title="Section 1"/>
+          
+          <Section2 title="Section 2"/>
+
+          <FormInput />
+
+          <Text style={styles.headerText}> Conditional Rendering</Text>
+          <Card title='Mon 17, April 2023' />
+          <Card title='Tue 18, April 2023' showButton={true} />
+          <Card title='Wed 19, April 2023' showButton={true} buttonTitle='Click me also'/>
+          
+          <UserDisplay error={true} />
+          <UserDisplay loading={true} title='Data Successfully Loaded' />
+          <UserDisplay title='Data Successfully Loaded!!' />
+        </View>
+      </ScrollView>
+
       {/* list */}
       {/* i had to bring Flatlist outside ScrollView because both have scroll feature, which will clash */}
       {/* move to a new screen to test */}
-      <View>
+      {/* <View style={styles.head}>
+        <Text style={styles.headerText}>List and Keys</Text>
+      </View> */}
+      {/* <View>
         <FlatList
         horizontal
         data={names}
@@ -57,7 +151,7 @@ export default function App() {
           );
         }}
         />
-      </View>
+      </View> */}
     </SafeAreaView>
 
     
